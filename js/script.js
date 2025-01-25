6b2209b3d16099fa5e444b47f82fced8b87302c0
bikramkumarmohapatra232 <bikramkumarmohapatra232@gmail.com>
2025-01-25 08:20:54 +0530

    adding files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Simple script for form submission (you can expand it as needed)
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for your message! We will get back to you soon.");
+});
